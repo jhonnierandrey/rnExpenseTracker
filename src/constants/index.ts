de54be1f8c8127ac19ec72469cd2573b2b9795ec
jhonnierandrey <jhonnierandrey@outlook.com>
2023-09-05 18:44:42 -0300

    0905 - replacing images with icons

---
+++ src/constants/index.ts
@@ -0,0 +1,34 @@
+export const APP_ICONS = {
+  add: {
+    icon: 'plus-thick',
+    color: '#38bdf8',
+  },
+  investment: {
+    icon: 'chart-line',
+    color: '#3b82f6',
+  },
+  food: {
+    icon: 'food',
+    color: '#db2777',
+  },
+  home: {
+    icon: 'home-outline',
+    color: '#4f46e5',
+  },
+  miscellaneous: {
+    icon: 'receipt',
+    color: '#34d399',
+  },
+  fun: {
+    icon: 'party-popper',
+    color: '#fbbf24',
+  },
+  health: {
+    icon: 'google-fit',
+    color: '#ef4444',
+  },
+  subscriptions: {
+    icon: 'youtube-subscription',
+    color: '#7c3aed',
+  },
+};
